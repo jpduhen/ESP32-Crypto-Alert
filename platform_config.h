c8ece3fb63e7694677118b226ec1e2e658cb57ea
jpduhen <45515609+jpduhen@users.noreply.github.com>
2026-01-07 21:52:11 +0100

    Update version minor to 31

--- platform_config.h
+++ platform_config.h
@@ -18,8 +18,8 @@
 // Versie wordt hier gedefinieerd zodat het beschikbaar is voor alle modules
 #ifndef VERSION_STRING
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 30
-#define VERSION_STRING "4.30"
+#define VERSION_MINOR 31
+#define VERSION_STRING "4.31"
 #endif
 
 // --- Debug Configuration ---
