4e6927e5e98885d511b1a985437d24ec85830c51
jpduhen <45515609+jpduhen@users.noreply.github.com>
2026-01-09 15:23:33 +0100

    Update version to 4.37

--- platform_config.h
+++ platform_config.h
@@ -18,8 +18,8 @@
 // Versie wordt hier gedefinieerd zodat het beschikbaar is voor alle modules
 #ifndef VERSION_STRING
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 36
-#define VERSION_STRING "4.36"
+#define VERSION_MINOR 37
+#define VERSION_STRING "4.37"
 #endif
 
 // --- Debug Configuration ---
