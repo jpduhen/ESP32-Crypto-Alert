28fc11b10b75fefdb1fe4a5a4711030e7c4553d1
jpduhen <45515609+jpduhen@users.noreply.github.com>
2026-01-09 17:25:09 +0100

    Update version to 4.42

--- platform_config.h
+++ platform_config.h
@@ -18,8 +18,8 @@
 // Versie wordt hier gedefinieerd zodat het beschikbaar is voor alle modules
 #ifndef VERSION_STRING
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 41
-#define VERSION_STRING "4.41"
+#define VERSION_MINOR 42
+#define VERSION_STRING "4.42"
 #endif
 
 // --- Debug Configuration ---
